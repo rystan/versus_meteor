578deea01ae0dd7d7c7628446c7879e9c89e316f
Michael Simons <rystan@gmail.com>
2014-10-30 16:31:16 -0400

    fixing meteor errors build errors

--- versus.js
+++ versus.js
@@ -1,8 +1,8 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
-  Session.setDefault("thumb_left", "img/mike_thumb.jpg");
-  Session.setDefault("thumb_right", "img/ben_thumb.jpg");
+  Session.setDefault("thumb_left", "./img/mike_thumb.jpg");
+  Session.setDefault("thumb_right", "./img/ben_thumb.jpg");
 
   Session.setDefault("name_left", "Mike Simons");
   Session.setDefault("name_right", "Ben Rood");
